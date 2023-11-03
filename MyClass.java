a5efbae1239c4dc1264f0ef21d8062a11fe55f77
titumama1672 <149819231+titumama1672@users.noreply.github.com>
2023-11-04 00:58:07 +0530

    Add files via upload

---
+++ MyClass.java
@@ -0,0 +1,9 @@
+package mypackage;
+
+public class MyClass {
+    public static void main(String[] args) {
+    public void display(){
+        System.out.println("Hello, World!");
+    }
+}
+}
---
+++ importpackage.java
@@ -0,0 +1,11 @@
+package input;
+
+import mypackage.MyClass;
+
+public class importpackage{
+public static void main(String []args){
+MyClass m = new MyClass();
+m.display();
+}
+
+}
